46a260d9d9f6f76b603534e7c96162f6ce5d6bd1
Alya <96687229+alya-tok@users.noreply.github.com>
2023-08-01 21:01:45 +0700

    update twitter.js

--- plugins/download/twitter.js
+++ plugins/download/twitter.js
@@ -14,7 +14,7 @@ exports.run = {
          if (!args || !args[0]) return client.reply(m.chat, Func.example(isPrefix, command, 'https://twitter.com/mosidik/status/1475812845249957889?s=20'), m)
          if (!args[0].match(/(twitter.com)/gi)) return client.reply(m.chat, global.status.invalid, m)
          client.sendReact(m.chat, '🕒', m.key)
-         let json = await Func.fetchJson(API('alya', '/api/twitter', { url: args[0] }, 'apikey'), { 'referer': 'https://savetwitter.net' })
+         let json = await Func.fetchJson(API('alya', '/api/twitter', { url: args[0] }, 'apikey'))
          if (!json.status) return client.reply(m.chat, Func.jsonFormat(json), m)
          json.data.map(async v => {
          client.sendFile(m.chat, v.url, '', '', m)
