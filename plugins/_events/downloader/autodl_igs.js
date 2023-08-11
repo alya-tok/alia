e56b04d06d08e8110376cae9d515c2f87fb96ffe
apriliaez46@gmail.com <apriliaez46@gmail.com>
2023-08-11 08:06:45 +0700

    fix

--- plugins/_events/downloader/autodl_igs.js
+++ plugins/_events/downloader/autodl_igs.js
@@ -1,40 +1,41 @@
 exports.run = {
-   regex: /^(?:https?:\/\/)?(?:www\.)?(?:instagram\.com\/)(?:stories\/)(?:\S+)?$/,
-   async: async (m, {
-      client,
-      body,
-      users,
-      setting
-   }) => {
-      try {
-         const regex = /^(?:https?:\/\/)?(?:www\.)?(?:instagram\.com\/)(?:stories\/)(?:\S+)?$/;
-         const extract = body ? Func.generateLink(body) : null
-         if (extract) {
-            const links = extract.filter(v => v.match(regex))
-            if (links.length != 0) {
-               if (users.limit > 0) {
-                  let limit = 1
-                  if (users.limit >= limit) {
-                     users.limit -= limit
-                  } else return client.reply(m.chat, Func.texted('bold', `🚩 Limit Anda tidak cukup untuk menggunakan fitur ini.`), m)
-               }
-               client.sendReact(m.chat, '🕒', m.key)
-               let old = new Date()
-               Func.hitstat('igs', m.sender)
-               links.map(async link => {
-               let json = await Func.fetchJson(API('alya', '/api/igs', { url: link }, 'apikey'))
-               if (!json.status) return client.reply(m.chat, Func.jsonFormat(json), m)
-               for (let v of json.data) {
-               client.sendFile(m.chat, `v.urFetching* : ${((new Date - old) * 1)} ms (${i+1})`, m)
-                     await Func.delay(1500)
-                  }
-               })
+  regex: /^(?:https?:\/\/)?(?:www\.)?(?:instagram\.com\/)(?:stories\/)(?:\S+)?$/,
+  async: async (m, {
+    client,
+    body,
+    users,
+    setting,
+    scrap
+  }) => {
+    try {
+      const regex = /^(?:https?:\/\/)?(?:www\.)?(?:instagram\.com\/)(?:stories\/)(?:\S+)?$/;
+      const extract = body ? Func.generateLink(body) : null
+      if (extract) {
+        const links = extract.filter(v => v.match(regex))
+        if (links.length != 0) {
+          if (users.limit > 0) {
+            let limit = 1
+            if (users.limit >= limit) {
+              users.limit -= limit
+            } else return client.reply(m.chat, Func.texted('bold', `🚩 Limit Anda tidak cukup untuk menggunakan fitur ini.`), m)
+          }
+          client.sendReact(m.chat, '🕒', m.key)
+          let old = new Date()
+          Func.hitstat('igs', m.sender)
+          links.map(async link => {
+            let json = await Func.fetchJson(API('alya', '/api/igs', { url: link }, 'apikey'))
+            if (!json.status) return client.reply(m.chat, Func.jsonFormat(json), m)
+            for (let v of json.data) {
+              client.sendFile(m.chat, v.url, ``, `🍟 *Fetching* : ${((new Date - old) * 1)} ms (${i+1})`, m)
+              await Func.delay(1500)
             }
-         }
-      } catch (e) {
-         return client.reply(m.chat, Func.jsonFormat(e), m)
+          })
+        }
       }
-   },
-   limit: true,
-   download: true,
+    } catch (e) {
+      return client.reply(m.chat, Func.jsonFormat(e), m)
+    }
+  },
+  limit: true,
+  download: true,
 }
